c63d08b11a87722676b0f6cf0ad69d6fd292298e
unknown <gsn181200@gmail.com>
2020-03-15 15:39:52 -0300

    Gustavo

--- src/main/java/com/example/ac1/controller/ProductController.java
+++ src/main/java/com/example/ac1/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.example.ac1.controller;
 import java.util.HashMap;
 
 import com.example.ac1.model.Product;
+import com.example.ac1.service.ProductService;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,16 +17,42 @@ import org.springframework.web.servlet.ModelAndView;
  */
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/app/produto")
 public class ProductController {
 
-    @GetMapping("/produtos")
-    public ModelAndView getProducts()
-    {
-        ModelAndView mv = new ModelAndView("productView");
-        HashMap<String, Product> prod;
-        
+    @Autowired
+    private ProductService ps;
 
+    @GetMapping("/{id}")
+    public ModelAndView getProductByIdControl(@PathVariable("id") int id)
+    {
+        ModelAndView mv = new ModelAndView("productByIdView");
+        Product produtoById = ps.getProductByIdService(id);
+        mv.addObject("produto", produtoById);
+        return mv;
+    }
+
+    @GetMapping("/inStock")
+    public ModelAndView getProductByInStockControl()
+    {
+        ModelAndView mv = new ModelAndView("productByInStockView");
+        mv.addObject("produto", ps.getProductByInStock().values());
+        return mv;
+    }
+
+    @GetMapping("/above/{value}")
+    public ModelAndView getProductByAboveValue(@PathVariable("value") double value)
+    {
+        ModelAndView mv = new ModelAndView("productByAboveValueView");
+        mv.addObject("produto", ps.getProductByAboveValue(value).values());
+        return mv;
+    }
+
+    @GetMapping("/below/{value}")
+    public ModelAndView getProductByBelowValue(@PathVariable("value") double value)
+    {
+        ModelAndView mv = new ModelAndView("productByBelowValueView");
+        mv.addObject("produto", ps.getProductByBelowValue(value).values());
         return mv;
     }
 
--- src/main/java/com/example/ac1/service/ProductService.java
+++ src/main/java/com/example/ac1/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.example.ac1.service;
 
 import com.example.ac1.repository.ProductRepository;
+
+import java.util.HashMap;
+
 import com.example.ac1.model.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +24,36 @@ public class ProductService {
         return prod;
     }
     
+    public HashMap<Integer, Product> getProductByInStock()
+    {
+        int cont = 0;
+        HashMap<Integer, Product> produtoInStock = new HashMap<>(), product;
+        product = pr.getProdutos();
+        for(Product produtoAux: product.values())
+            if(produtoAux.getEstoque() > 0)
+                produtoInStock.put(cont++, produtoAux);
+        return produtoInStock;
+    }
+
+    public HashMap<Integer, Product> getProductByAboveValue(double valor)
+    {
+        int cont = 0;
+        HashMap<Integer, Product> produtoAboveValue = new HashMap<>(), produtoAux;
+        produtoAux = pr.getProdutos();
+        for(Product productAux: produtoAux.values())
+            if(productAux.getValor() > valor)
+                produtoAboveValue.put(cont++, productAux);
+        return produtoAboveValue;
+    }
+
+    public HashMap<Integer, Product> getProductByBelowValue(double valor)
+    {
+        int cont = 0;
+        HashMap<Integer, Product> produtoBelowValue = new HashMap<>(), produtoAux;
+        produtoAux = pr.getProdutos();
+        for(Product productAux: produtoAux.values())
+            if(productAux.getValor() < valor)
+                produtoBelowValue.put(cont++, productAux);
+        return produtoBelowValue;
+    }
 }
